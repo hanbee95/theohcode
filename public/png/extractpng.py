d353db3bcee75284c3310eda03c2817d379cc11a
hanbee95 <nospicynocilantro@gmail.com>
2022-10-05 20:19:19 -0500

    added photos to the website

--- public/png/extractpng.py
+++ public/png/extractpng.py
@@ -2,12 +2,14 @@ import re
 import wget
 
 f = open("champion.json", "r", encoding="utf8")
+fl = open('champlist.txt', 'w')
 reading = f.read()
 png = re.findall("full\": \"(.*?)\"", reading, re.DOTALL)
 
 photofile = []
 for elem in png:
     url = 'http://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/'+elem
-    image_filename = wget.download(url)
-    photofile.append(image_filename)
-print(photofile)
+    fl.writelines('<img src=png/'+elem+" class=\"sqimg\">"+'\n')
+    # image_filename = wget.download(url)
+    # photofile.append(image_filename)
+# print(photofile)
