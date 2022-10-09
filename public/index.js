6f90e4205d48426bd7d4533713c88fecbe7fd7c3
hanbee95 <nospicynocilantro@gmail.com>
2022-10-08 23:13:55 -0500

    use different lane box activation

--- public/index.js
+++ public/index.js
@@ -1,7 +1,15 @@
+var activestate = 'top';
+
+
 function openImg(t){
     var img = document.createElement("img");
     img.src = t;
     console.log(t);
-    var div = document.getElementById("top");
+    var div = document.getElementById(activestate);
     div.appendChild(img);
+}
+
+function changelane(lane){
+    activestate = lane;
+    console.log(activestate);
 }
