6fc2c0ad7cee7dd5156eaf75e230c6652712d75c
hanbee95 <nospicynocilantro@gmail.com>
2022-10-11 07:54:03 -0500

    active region yellow

--- public/index.js
+++ public/index.js
@@ -1,15 +1,36 @@
-var activestate = 'top';
+// const { ids } = require("webpack");
 
+var activestate = 'top';
+let lanes = ['ban','top','jg','mid','adc','sup'];
+let champlanedict = {
+                        'ban':[], 
+                        'top':[], 
+                        'jg':[],
+                        'mid':[],
+                        'adc':[],
+                        'sup':[]
+                    };
 
 function openImg(t){
     var img = document.createElement("img");
     img.src = t;
-    console.log(t);
     var div = document.getElementById(activestate);
     div.appendChild(img);
+    // var tmp = champlanedict[div.id];
+    console.log(t)
+    console.log(div.id)
 }
 
 function changelane(lane){
     activestate = lane;
-    console.log(activestate);
+    for (elem of lanes){
+        if (elem === lane){
+            document.getElementById(elem).style.backgroundColor= 'yellow';
+        } 
+        else { 
+            document.getElementById(elem).style.backgroundColor = 'white';
+        }
+    }
+   
+    // console.log(activestate);
 }
