c312acd09ec7f3b227de26b2d47141f86231144b
hanbee95 <nospicynocilantro@gmail.com>
2022-10-26 19:33:30 -0500

    src possiblet to upload/download db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { deleteObject, getDownloadURL, getStorage, uploadBytes, uploadString } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-storage.js";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js';
+import { getFirestore, addDoc, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +21,31 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+  if(user != null){
+    console.log('logged in!');
+  } else{
+    console.log('No user');
+  }
+});
+const db = getFirestore(app);
+
+try {
+  const docRef = await addDoc(collection(db, "product"), {
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+  });
+
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+const querySnapshot = await getDocs(collection(db, "product"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
