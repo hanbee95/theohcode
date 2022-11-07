841a871f1168949f9f7ae2b801def1f95ec451b2
hanbee95 <nospicynocilantro@gmail.com>
2022-11-06 21:23:19 -0600

    google login!

--- src/test.js
+++ src/test.js
@@ -17,7 +17,9 @@ import {
   signOut,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  connectAuthEmulator
+  connectAuthEmulator,
+  GoogleAuthProvider,
+  signInWithPopup
 } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js';
 
 
@@ -34,6 +36,33 @@ const firebaseConfig = {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  //Google Login
+  const googlelogin = () =>{
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+    
+
+  }
+  
+
 // Login using email/password
 const loginEmailPassword = async () => {
     
@@ -90,7 +119,7 @@ const monitorAuthState = async () => {
 const logout = async () => {
   await signOut(auth);
 }
-
+btngooglelogin.addEventListener("click", googlelogin) 
 btnLogin.addEventListener("click", loginEmailPassword) 
 btnSignup.addEventListener("click", createAccount)
 btnLogout.addEventListener("click", logout)
