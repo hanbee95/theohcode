11e3b9202fe84fb47f3e8e720ea5e8cfdaf07381
hanbee95 <nospicynocilantro@gmail.com>
2022-11-17 23:21:06 -0600

    added updated version in automated way

--- public/png/extractpng.py
+++ public/png/extractpng.py
@@ -1,22 +1,53 @@
 import re
 import wget
 
-f = open("champion.json", "r", encoding="utf8")
+# f = open("champion.json", "r", encoding="utf8")
 fl = open('champlist.txt', 'w')
-reading = f.read()
-png = re.findall("full\": \"(.*?)\"", reading, re.DOTALL)
+# reading = f.read()
+# png = re.findall("full\": \"(.*?)\"", reading, re.DOTALL)
 
+import json
+
+import urllib.request
+with urllib.request.urlopen('https://ddragon.leagueoflegends.com/api/versions.json') as response:
+   html = response.read()
+# print(json.loads(html)[0])
+latestversion = json.loads(html)[0]
+
+with urllib.request.urlopen('http://ddragon.leagueoflegends.com/cdn/'+latestversion+'/data/en_US/champion.json') as response:
+   html = response.read()
+# print(json.loads(html))
+championjson = json.loads(html)
+champlist =championjson["data"].keys()
+# print(type(championjson))
+# # print(json.loads(html)[0])
+# pnglist = re.findall("full\': \'(.*?)\'", championjson, re.DOTALL)
+
+print(champlist)
 photofile = []
-for elem in png:
-    url = 'http://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/'+elem
+for champ in champlist:
+   url = 'http://ddragon.leagueoflegends.com/cdn/'+latestversion+'/img/champion/'+champ+'.png'
+   image_filename = wget.download(url)
+   photofile.append(image_filename)
+print(photofile)  
+
+for champ in champlist:
     fl.writelines('<div class="leftimages">'+'\n')
-    fl.writelines('  <img src=png/'+elem+" class=\"sqimg\" onclick=\"openImg(this.src)\">"+'\n')
-    fl.writelines('  <span class="championname">'+elem.replace('.png','')+'</span>'+'\n')
+    fl.writelines('  <img src=../public/png/'+champ+'.png'+" class=\"sqimg\" onclick=\"openImg(this.src)\">"+'\n')
+    fl.writelines('  <span class="championname">'+champ+'</span>'+'\n')
     fl.writelines('</div>'+'\n')
-    # image_filename = wget.download(url)
-    #    <div class="leftimages">
-    #     <img src=png/Aatrox.png id='Aatrox' class="sqimg" onclick="openImg(this.src)">
-    #     <span class="championname">Aatrox</span>
-    #   </div>
-    # photofile.append(image_filename)
-# print(photofile)
+#    
+# photofile = []
+# for elem in png:
+#     url = 'http://ddragon.leagueoflegends.com/cdn/12.19.1/img/champion/'+elem
+#     fl.writelines('<div class="leftimages">'+'\n')
+#     fl.writelines('  <img src=../public/png/'+elem+" class=\"sqimg\" onclick=\"openImg(this.src)\">"+'\n')
+#     fl.writelines('  <span class="championname">'+elem.replace('.png','')+'</span>'+'\n')
+#     fl.writelines('</div>'+'\n')
+#     # image_filename = wget.download(url)
+#     #    <div class="leftimages">
+#     #     <img src=png/Aatrox.png id='Aatrox' class="sqimg" onclick="openImg(this.src)">
+#     #     <span class="championname">Aatrox</span>
+#     #   </div>
+#     # photofile.append(image_filename)
+# # print(photofile)
